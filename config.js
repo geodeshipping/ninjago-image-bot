024b99b00bef52a679059ba429c262e6bdcb59e6
geodeshipping <63154332+geodeshipping@users.noreply.github.com>
2020-04-04 15:33:25 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         '6P5O0B8M5gru39XK21LvyYQy9',
+  consumer_secret:      'zt2a5H95wnt3r0Hl9ysUwvdlEELJoTZvd36dHuJqa8PfK9G6T2',
+  access_token:         '1246406208036261888-thayihucUpI0zRFoIWBN5OotYFwkGZ',
+  access_token_secret:  'HehdOwufbNcMIq1tYZdcrfNb2FpoRgMXWy8Ly5coEsYQy'
+}
+
+module.exports = config;
